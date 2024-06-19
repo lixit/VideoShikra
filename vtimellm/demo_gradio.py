254130d4fb93263915e032047e4dcee71c8ccd29
Xitong Li <lixiton@gmail.com>
2024-06-19 18:47:18 -0400

    display the result bbox in Gradio

--- vtimellm/demo_gradio.py
+++ vtimellm/demo_gradio.py
@@ -2,6 +2,9 @@
 Adapted from: https://github.com/Vision-CAIR/MiniGPT-4/blob/main/demo.py 
 """
 import argparse
+import re
+import ast
+import cv2
 import os
 root_dir = os.path.join(os.getcwd(), "..")
 import sys
@@ -101,7 +104,10 @@ def upload_video(gr_video, chat_state, video_features_state, conv_state, chatbot
         chat_state = conv_templates["v1"].copy()
         conv_state['first'] = True
 
-        return gr.update(interactive=False), gr.update(interactive=True, placeholder='Type and press Enter'), gr.update(value="Start Chatting", interactive=False), chat_state, video_features_state, conv_state, chatbot
+        return gr.update(interactive=False), \
+            gr.update(interactive=True, placeholder='Type and press Enter'), \
+            gr.update(value="Start Chatting", interactive=False), \
+            chat_state, video_features_state, conv_state, chatbot
 
 def gradio_ask(user_message, chatbot, chat_state, conv_state):
     if len(user_message) == 0:
@@ -147,11 +153,71 @@ def gradio_answer(chatbot, chat_state, video_features_state, conv_state, tempera
     print(chat_state)
     return chatbot, chat_state
 
+def extract_frame_and_position(output: str):
+    frame_pattern = re.compile(r'frame (\d+)')
+    bbox_pattern = re.compile(r'\[(\d+\.\d+), (\d+\.\d+), (\d+\.\d+), (\d+\.\d+)\]')
+    
+    frame = frame_pattern.findall(output)
+    bbox = bbox_pattern.findall(output)
+    
+    frame = [int(f) for f in frame]
+    bbox = [[float(b) for b in box] for box in bbox]
+    
+    return list(zip(frame, bbox))
+
+def display_images(video_path, chat_state, N=100):
+    
+    answer = chat_state.messages[-1][-1]
+    
+    results = extract_frame_and_position(answer)
+    
+    frame0 = results[0][0]
+    bbox0 = results[0][1]
+    frame1 = results[1][0]
+    bbox1 = results[1][1]
+
+    cap = cv2.VideoCapture(video_path)
+    total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    real_frame0 = int(frame0 * total_frames / N)
+    real_frame1 = int(frame1 * total_frames / N)
+    
+    cap.set(cv2.CAP_PROP_POS_FRAMES, real_frame0)
+    
+    ret, frame1 = cap.read()
+    if ret:
+        height, width = frame1.shape[:2]
+        
+        xmin = int(bbox0[0] * width)
+        ymin = int(bbox0[1] * height)
+        xmax = int(bbox0[2] * width)
+        ymax = int(bbox0[3] * height)
+        
+        cv2.rectangle(frame1, (xmin, ymin), (xmax, ymax), (0, 255, 0), 2)
+        frame1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2RGB)
+
+        
+    cap.set(cv2.CAP_PROP_POS_FRAMES, real_frame1)
+    ret, frame2 = cap.read()
+    if ret:
+        height, width = frame2.shape[:2]
+        
+        xmin = int(bbox1[0] * width)
+        ymin = int(bbox1[1] * height)
+        xmax = int(bbox1[2] * width)
+        ymax = int(bbox1[3] * height)
+        
+        cv2.rectangle(frame2, (xmin, ymin), (xmax, ymax), (0, 255, 0), 2)
+        frame2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2RGB)
+        
+    cap.release()
+    
+    return frame1, frame2
+
 with gr.Blocks() as demo:
     gr.Markdown('''# Demo for VTimeLLM''')
 
     with gr.Row():
-        with gr.Column(scale=0.5):
+        with gr.Column():
             video = gr.Video()
 
             upload_button = gr.Button(value="Upload & Start Chat", interactive=True, variant="primary")
@@ -173,13 +239,24 @@ with gr.Blocks() as demo:
             text_input = gr.Textbox(label='User', placeholder=TEXT_PLACEHOLDER, interactive=False)
             
 
-    with gr.Column():
-        gr.Examples(examples=[
-            [os.path.join(root_dir, f"images/demo.mp4"), "Explain why the video is funny."],
-        ], inputs=[video, text_input])
+#    with gr.Column():
+#        gr.Examples(examples=[
+#            [os.path.join(root_dir, f"images/demo.mp4"), "Explain why the video is funny."],
+#        ], inputs=[video, text_input])
         
-    upload_button.click(upload_video, [video, chat_state, video_features_state, conv_state, chatbot], [video, text_input, upload_button, chat_state, video_features_state, conv_state, chatbot])
-    text_input.submit(gradio_ask, [text_input, chatbot, chat_state, conv_state], [text_input, chatbot, chat_state, conv_state]).then(gradio_answer, [chatbot, chat_state, video_features_state, conv_state, temperature], [chatbot, chat_state])
-    clear.click(gradio_reset, [chat_state, video_features_state, conv_state], [chatbot, video, text_input, upload_button, chat_state, video_features_state, conv_state], queue=False)
+    with gr.Row():
+        image1 = gr.Image(label="Image 1")
+        image2 = gr.Image(label="Image 2")
+        
+    upload_button.click(upload_video, [video, chat_state, video_features_state, conv_state, chatbot],
+                        [video, text_input, upload_button, chat_state, video_features_state, conv_state, chatbot])
+    text_input.submit(gradio_ask, [text_input, chatbot, chat_state, conv_state], 
+                      [text_input, chatbot, chat_state, conv_state])\
+                          .then(gradio_answer, 
+                                [chatbot, chat_state, video_features_state, conv_state, temperature], 
+                                [chatbot, chat_state])\
+                            .then(display_images, [video, chat_state], [image1, image2])
+    clear.click(gradio_reset, [chat_state, video_features_state, conv_state], 
+                [chatbot, video, text_input, upload_button, chat_state, video_features_state, conv_state], queue=False)
 
 demo.queue().launch(share=args.share)
