39d7ef0eab4930ca1f94e30f033b170efba905e1
Xitong Li <lixiton@gmail.com>
2024-06-26 09:53:43 -0400

    add stage3 training script

--- scripts/stage3.sh
+++ scripts/stage3.sh
@@ -4,26 +4,27 @@ MODEL_VERSION=vicuna-v1-5-7b
 gpu_vis=0 # per_device_train_batch_size * gradient_accumulation_steps * n_gpus = 128
 MASTER_PORT=29570
 
+cd ..
 
 deepspeed --include localhost:$gpu_vis --master_port $MASTER_PORT vtimellm/train/train_mem.py \
     --deepspeed ./scripts/zero2.json \
     --lora_enable True \
     --training_stage 3 \
-    --model_name_or_path ./checkpoints/vicuna-7b-v1.5 \
+    --model_name_or_path lmsys/vicuna-7b-v1.5 \
     --version v1 \
-    --data_path ./data/stage3.json \
-    --feat_folder /path/to/stage3_feat \
+    --data_path ./data/xl/results_multithread.json \
+    --feat_folder ./data/xl/stage4_features  \
     --pretrain_mm_mlp_adapter ./checkpoints/vtimellm-$MODEL_VERSION-stage1/mm_projector.bin \
     --stage2_path ./checkpoints/vtimellm-$MODEL_VERSION-stage2 \
-    --output_dir ./checkpoints/vtimellm-$MODEL_VERSION-stage3 \
+    --output_dir ./checkpoints/vtimellm-$MODEL_VERSION-stage3_xl_300_epoch \
     --bf16 True \
-    --num_train_epochs 2 \
+    --num_train_epochs 300 \
     --per_device_train_batch_size 8 \
     --gradient_accumulation_steps 16 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
-    --save_steps 50000 \
-    --save_total_limit 1 \
+    --save_steps 100 \
+    --save_total_limit 5 \
     --learning_rate 1e-4 \
     --freeze_mm_mlp_adapter True \
     --lora_r 64 \
